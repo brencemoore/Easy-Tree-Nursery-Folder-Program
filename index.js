54e335978e251d18e953f716e1b91afd5de70560
brencemoore <brence.exd23@gmail.com>
2026-07-14 00:51:28 -0500

    Added logos

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function createWindow() {
         minWidth: 400,
         minHeight: 400,
 
+        icon: path.join(__dirname, "assets", "simple_logo", "favicon.ico"),
+
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             contextIsolation: true,
